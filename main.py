b695ddb8bb869956ebf241f6c33ea2333840903b
Aeyla Naseer <aeylanaseer@gmail.com>
2025-02-25 20:29:23 +0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,88 @@
+# Printing Hello-World!
+
+print("Hello world!!")
+
+# ---------------------------------------------------------------
+# --------------------------------
+
+# DATA TYPES & THEIR EXAMPLES
+ # 1. Integers
+int1 = 10
+int2 = 20
+
+ # 2. Floating-point numbers
+float1 = 10.5
+float2 = 20.75
+
+ # 3. Strings
+str1 = "Hello"
+str2 = "World!"
+
+ # 4. Booleans (True/False)
+bool1 = True
+bool2 = False
+
+ # 5. Lists
+list1 = [1, 2, 3, 4, 5]
+list2 = ["Apple", "Banana", "Cherry"]
+
+ # 6. Tuples
+tuple1 = (1, 2, 3, 4, 5)
+tuple2 = ("Apple", "Banana", "Cherry")
+
+ # 7. Dictionaries
+dict1 = {"Name": "John", "Age": 30}
+dict2 = {"Color": "Red", "Shape": "Square"}
+
+ # 8. Sets
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+# NOW PRINTING THE DATA TYPES!!
+
+# Printing the variables
+
+print("Integer 1:", int1)
+print("Integer 2:", int2)
+print("Floating-point number 1:", float1)
+print("Floating-point number 2:", float2)
+print("String 1:", str1)
+
+# Printing the boolean values
+
+print("Boolean 1:", bool1)
+print("Boolean 2:", bool2)
+
+# Printing the lists
+
+print("List 1:", list1)
+print("List 2:", list2)
+
+# Printing the tuples
+
+print("Tuple 1:", tuple1)
+print("Tuple 2:", tuple2)
+
+# Printing the dictionaries
+
+print("Dictionary 1:", dict1)
+print("Dictionary 2:", dict2)
+
+# Printing the sets
+
+print("Set 1:", set1)
+print("Set 2:", set2)
+
+# PRINTED DATA TYPES SUCCESSFULLY 
+
+# Type checking
+
+print("Type of int1:", type(int1))
+print("Type of float1:", type(float1))
+print("Type of str1:", type(str1))
+print("Type of bool1:", type(bool1))
+print("Type of list1:", type(list1))
+
+# TYPE-CHECKED!!
+#  x--------------------------------------------------------------------x
+
